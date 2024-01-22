c84738aa8887439fd720066419155d17f7e2c918
peace645 <peace645@gmail.com>
2024-01-22 18:27:32 +0200

    Create deep.py
    program 1   cs50

---
+++ deep.py
@@ -0,0 +1,5 @@
+response=input("What is the answer to the great question of life, the universe and everything? ")
+if(response=='42' or response=="forty-two" or response=="forty two"):
+    print("Yes")
+else :
+  print("No")
